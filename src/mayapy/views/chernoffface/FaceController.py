c3559cae5e73c2ad338938b830a4118b17cecd2c
Peter McKay <pem@cs.uoregon.edu>
2015-11-28 15:35:52 -0800

    fixed some broken math

--- src/mayapy/views/chernoffface/FaceController.py
+++ src/mayapy/views/chernoffface/FaceController.py
@@ -1,6 +1,9 @@
 from nimble import cmds as mc
 from difflib import SequenceMatcher as sm
 
+#def normTwo(value):
+#    return abs(value)/100
+
 # newVal will be between -100 and 100, with negative/positive representing sad and angry respectively
 def eyeBrow(newVal):
     if newVal < 0:
@@ -9,23 +12,26 @@ def eyeBrow(newVal):
     else:
         suffix    =  "FaceDeformer.FaceAngry"
         nonSuffix = "FaceDeformer.FaceSad"
+    newVal = abs(newVal)/100
     mc.setAttr(nonSuffix,  0.0)
-    mc.setAttr(suffix,  ((newVal/100.0) + 1.0)/2.0))
+    mc.setAttr(suffix, newVal)
     
+# This one needs some work    
 def mouthEnbiggen(newVal):
-    name = "mouthSize.FaceBigMouth"
-    mc.setAttr(name,  ((newVal/100.0) + 1.0)/2.0))
+    newVal = abs(newVal)/100
+    name = "FaceDeformer.FaceBigMouth"
+    mc.setAttr(name,  newVal)
 
 def eyeEnbiggen(newVal):
-    newP=((newVal/100.0) + 1.0)/2.0 # new percentage
+    newP=abs(newVal)/100.0 # new percentage
     if newVal < 0:
         nonSuffix = "FaceDeformer.FaceBigEye"
         suffix    = "FaceDeformer.FaceSmallEye"
-        scaleV=newP + (1-newP)*.6 #woo guess what I learned in sciviz class?
+        scaleV = 1 - newP*.4 #woo guess what I learned in sciviz class?
     else:
         suffix    =  "FaceDeformer.FaceBigEye"
         nonSuffix = "FaceDeformer.FaceSmallEye"
-        scaleV=newP*1.5 + (1-newP) #woo guess what I learned in sciviz class?
+        scaleV = 1 + newP*.5 #woo guess what I learned in sciviz class?
     mc.setAttr(nonSuffix,  0.0)
     mc.setAttr(suffix,  newP)
     mc.setAttr("LeftEye.scaleZ", scaleV)
@@ -35,8 +41,7 @@ def eyeEnbiggen(newVal):
     mc.setAttr("RightEye1.scaleX", scaleV)
     mc.setAttr("RightEye1.scaleY", scaleV)
 
-# Face6, I know, it's bad, but Maya loses its shit when I try to
-# change the name of the thing.
+# smile/frown thing.
 def mouth(newVal):
     if newVal < 0:
         nonSuffix = "FaceDeformer.Face6"
@@ -44,55 +49,39 @@ def mouth(newVal):
     else:
         suffix    =  "FaceDeformer.Face6"
         nonSuffix = "FaceDeformer.FaceFrown"
+    newVal = abs(newVal)/100
     mc.setAttr(nonSuffix,  0.0)
-    mc.setAttr(suffix,  ((newVal/100.0) + 1.0)/2.0))
+    mc.setAttr(suffix,  newVal)
 
 
-
+# tall or wide head
 def head(newVal):
     if newVal < 0:
         nonSuffix = "FaceDeformer.FaceTall"
         suffix    = "FaceDeformer.FaceWide"
     else:
-        suffix    =  "FaceDeformer.Tall"
+        suffix    =  "FaceDeformer.FaceTall"
         nonSuffix = "FaceDeformer.FaceWide"
+    newVal = abs(newVal)/100
     mc.setAttr(nonSuffix,  0.0)
-    mc.setAttr(suffix,  ((newVal/100.0) + 1.0)/2.0))
+    mc.setAttr(suffix, newVal)
 
 
-#next up
+# wide or narrow eyes
 def eyeDistance(newVal):
-    newP=((newVal/100.0) + 1.0)/2.0 # new percentage
+    newP=abs(newVal)/100.0 # new percentage
     if newVal < 0:
         nonSuffix = "FaceDeformer.FaceWideEye"
         suffix    = "FaceDeformer.FaceNarrowEye"
-        scaleV=newP*-.25 #woo guess what I learned in sciviz class?
+        scaleV= newP*-.25 #woo guess what I learned in sciviz class?
     else:
         suffix    =  "FaceDeformer.FaceWideEye"
         nonSuffix = "FaceDeformer.FaceNarrowEye"
-        scaleV=newP*.5 #woo guess what I learned in sciviz class?
+        scaleV= newP*.5 #woo guess what I learned in sciviz class?
         
     mc.setAttr(nonSuffix,  0.0)
     mc.setAttr(suffix,  newP)
-    ltX = mc.getAttr("LeftEye.translateX")
-    rtX = mc.getAttr("RightEye1.translateX")
-    mc.setAttr("LeftEye.translateX", ltX+scaleV)
-    mc.setAttr("RightEye1.translateX", rtX-scaleV)
+    mc.setAttr("LeftEye.translateX", scaleV)
+    mc.setAttr("RightEye1.translateX", -1*scaleV)
 
-
-    
-#this might be extraneous, but I'm a keep it here until I double-check
-def findName(namelike):
-    objs = mc.ls()
-    bodyName = ""
-	compareScore = 0
-	newScore = 0
-    for nm in objs:
-		newScore = sm(None, nameLike, nm).ratio()
-		if newScore > compareScore:
-			compareScore = newScore
-			bodyName = nm
-	
-    print("query for: \"" + namelike + "\" returns: " + bodyName)
-    return bodyName
 
